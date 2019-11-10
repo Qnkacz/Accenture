bd3ca21a55bac2456217682b8eb581493ea281e7
ufus09@gmail.com <ufus09@gmail.com>
2019-11-11 00:39:46 +0100

    Add project files.

---
+++ Accenture/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Accenture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new Program();
+            Console.WriteLine("Welcome, please write aa sentence for me to reverse");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(Reverse(userInput));
+        }
+
+        #region Functions
+        private static string Reverse(string sentence)
+        {
+            try
+            {
+                string tmp = sentence.Trim();
+                string[] words = tmp.Split(' ');
+                Array.Reverse(words);
+                string output = string.Join(" ", words);
+                return output;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        #endregion
+
+    }
+}
